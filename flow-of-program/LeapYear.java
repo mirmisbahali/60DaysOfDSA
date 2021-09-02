e3cc475b8811e6bfa07d011d918a8971c73c91c5
mirmisbahali <>
2021-09-02 00:31:24 +0000

    Leap Year

---
+++ flow-of-program/LeapYear.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class LeapYear {
+  public static void main(String args[]) {
+    Scanner in = new  Scanner(System.in);
+    System.out.print("Enter a Year: ");
+    int year = in.nextInt();
+    if (year % 4 == 0)
+    {
+      if (year % 100 == 0)
+      {
+        if (year % 400 == 0)
+        {
+          System.out.println(year + " is a leap year");
+        }
+        else
+          System.out.println(year + " is not a leap year.");
+      }
+      else
+      {
+        System.out.println(year + " is a leap year");
+      }
+    }
+    else
+    {
+      System.out.println(year + " is not a leap year.");
+    }
+  }
+}
