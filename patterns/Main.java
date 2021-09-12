2c2fc45b67bdb25f081641b175861eb8b14f8b49
Mir Misbah Ali <67518304+mirmisbahali@users.noreply.github.com>
2021-09-12 14:04:09 +0000

    Day 10: Pattern 6

--- patterns/Main.java
+++ patterns/Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String args[]) {
-    problem5(5);
+    pattern6(5);
   }
-  static void problem1(int n) {
+  static void pattern1(int n) {
     for (int row = 0; row < n; row++) {
       for (int col = 0; col < n; col++) {
         System.out.print("* ");
@@ -11,7 +11,7 @@ class Main {
     }
   }
 
-  static void problem2(int n) {
+  static void pattern2(int n) {
     for (int row = 1; row <= n; row++) {
       for (int col = 1; col <= row; col++) {
         System.out.print("* ");
@@ -20,7 +20,7 @@ class Main {
     }
   }
 
-  static void problem3(int n) {
+  static void pattern3(int n) {
     for (int row = 1; row <= n; row++) {
       for (int col = 0; col <= n - row; col++) {
         System.out.print("* ");
@@ -29,7 +29,7 @@ class Main {
     }
   }
 
-  static void problem4(int n) {
+  static void pattern4(int n) {
     for (int row = 1; row <= n; row++) {
       for (int col = 1; col <= row; col++) {
         System.out.print(col + " ");
@@ -38,10 +38,24 @@ class Main {
     }
   }
 
-  static void problem5(int n) {
+  static void pattern5(int n) {
     for (int row = 1; row <= 2*n; row++) {
       int n_col = row > n ? (2*n - row) : row;
       for (int col = 1; col <= n_col; col++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern6(int n) {
+    for (int row = 1; row <= n; row++) {
+      int spaces = n-row;
+      int stars = row;
+      for (int i = 0; i < spaces; i++) {
+        System.out.print(" ");
+      }
+      for (int i = 0; i < stars; i++) {
         System.out.print("*");
       }
       System.out.println();
