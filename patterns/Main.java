c96d969d9aa468ba29ad3c8598a718ba0bc10ce7
Mir Misbah Ali <67518304+mirmisbahali@users.noreply.github.com>
2021-09-12 15:40:14 +0000

    Day 10: Pattern 12

--- patterns/Main.java
+++ patterns/Main.java
@@ -1,6 +1,21 @@
 class Main {
   public static void main(String args[]) {
-    pattern11(5);
+    pattern12(5);
+  }
+
+  static void pattern12(int n) {
+    for (int row = 1; row <= 2*n; row++) {
+      int spaces = row > n ? (2*n-row) : (row - 1);
+      int n_col = row > n ? (2*(n-spaces)) : (2*(n-row) + 1);
+
+      for (int i = 0; i < spaces; i++) System.out.print(" ");
+
+      for (int col = 0; col < n_col; col++) {
+        if (col % 2 == 0) System.out.print("*");
+        else System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 
   static void pattern11(int n) {
