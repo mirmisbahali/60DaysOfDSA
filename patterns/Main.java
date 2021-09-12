5d687c23a79f63b61672c1c035f037438f1eb92a
Mir Misbah Ali <67518304+mirmisbahali@users.noreply.github.com>
2021-09-12 14:21:34 +0000

    Day 10: Pattern 9

--- patterns/Main.java
+++ patterns/Main.java
@@ -1,7 +1,19 @@
 class Main {
   public static void main(String args[]) {
-    pattern8(5);
+    pattern9(5);
   }
+
+  static void pattern9(int n) {
+    for (int row = 1; row <= n; row++) {
+      int spaces = row-1;
+      int stars = 2*(n - row) + 1;
+      for (int i = 0; i < spaces; i++) System.out.print(" ");
+      for (int i = 0; i < stars; i++) System.out.print("*");
+      System.out.println();
+    }
+  }
+
+
   static void pattern1(int n) {
     for (int row = 0; row < n; row++) {
       for (int col = 0; col < n; col++) {
