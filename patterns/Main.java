b94e2f5d64bdf702e3005c11d176c4ae6c293ee5
Mir Misbah Ali <67518304+mirmisbahali@users.noreply.github.com>
2021-09-12 13:57:11 +0000

    Day 10: Pattern5

--- patterns/Main.java
+++ patterns/Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String args[]) {
-    problem3(5);
+    problem5(5);
   }
   static void problem1(int n) {
     for (int row = 0; row < n; row++) {
@@ -24,6 +24,25 @@ class Main {
     for (int row = 1; row <= n; row++) {
       for (int col = 0; col <= n - row; col++) {
         System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+
+  static void problem4(int n) {
+    for (int row = 1; row <= n; row++) {
+      for (int col = 1; col <= row; col++) {
+        System.out.print(col + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  static void problem5(int n) {
+    for (int row = 1; row <= 2*n; row++) {
+      int n_col = row > n ? (2*n - row) : row;
+      for (int col = 1; col <= n_col; col++) {
+        System.out.print("*");
       }
       System.out.println();
     }
