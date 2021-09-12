34f404814eba29cf6aeee49b986547601eef3a37
Mir Misbah Ali <67518304+mirmisbahali@users.noreply.github.com>
2021-09-12 13:50:41 +0000

    Day 10: Pattern3

--- patterns/Main.java
+++ patterns/Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String args[]) {
-    problem2(5);
+    problem3(5);
   }
   static void problem1(int n) {
     for (int row = 0; row < n; row++) {
@@ -14,6 +14,15 @@ class Main {
   static void problem2(int n) {
     for (int row = 1; row <= n; row++) {
       for (int col = 1; col <= row; col++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+
+  static void problem3(int n) {
+    for (int row = 1; row <= n; row++) {
+      for (int col = 0; col <= n - row; col++) {
         System.out.print("* ");
       }
       System.out.println();
